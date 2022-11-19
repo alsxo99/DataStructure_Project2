b99120a0ddffea6879a79c4b42a88aa7f0b524f3
alsxo99 <alsxo99@snu.ac.kr>
2022-11-19 07:47:35 +0000

    avltree not done

--- AVLTree/avltree.hpp
+++ AVLTree/avltree.hpp
@@ -57,6 +57,7 @@ class AVLTree {
         int getBalance(AVLNode<T,U>*& node);
         AVLNode<T,U>* rotate_left(AVLNode<T,U>*& node);
         AVLNode<T,U>* rotate_right(AVLNode<T,U>*& node);
+        AVLNode<T,U>* min_value_node(AVLNode<T,U>*& node);
 
         AVLNode<T,U>* insert(AVLNode<T,U>*& node, const T& key, const U& value);
         U search(AVLNode<T,U>*& node, const T& key);
@@ -94,90 +95,91 @@ bool AVLTree<T,U>::remove(const T& key) {
     return true;
 }
 
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::rotate_left(AVLNode<T,U>*& node){
     //TODO
-    //target node의 parent node 정보도 바꿔야하지 않나?
-    //target node의 parent node가 없을 경우, node->right이 새로운 root가 되어야한다.
     AVLNode<T,U> * right_child = node->right;
+    
     node->right = right_child->left;
-    if (root == node)
-        root = right_child;
     right_child->left = node;
 
     // update height
-    node->height = max(node->left->height, node->right->height) + 1;
-    right_child->height = max(right_child->left->height, right_child->right->height) + 1
-
+    node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
+    right_child->height = max(getHeight(right_child->left), getHeight(right_child->right)) + 1;
+    
     return right_child;
-    //delete 해야하나?
-
 }
 
 template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::rotate_right(AVLNode<T,U>*& node){
     //TODO
     AVLNode<T,U> * left_child = node->left;
+    
     node->left = left_child->right;
-    if (root == node)
-        root = left_child;
     left_child->right = node;
 
     // update height
-    node->height = max(node->left->height, node->right->height) + 1;
-    left_child->height = max(left_child->left->height, left_child->right->height) + 1;
+    node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
+    left_child->height = max(getHeight(left_child->left), getHeight(left_child->right)) + 1;
 
     return left_child;
 }
 
 template<typename T, typename U>
+AVLNode<T,U>* AVLTree<T,U>::min_value_node(AVLNode<T,U>*& node) {
+    
+    AVLNode<T,U>* current = node;
+
+    while (current->left)
+        current = current->left;
+    
+    return current;
+}
+
+template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::insert(AVLNode<T,U>*& node, const T& key, const U& value) {
     //TODO
-    //먼저, insert 할 position을 찾아야한다.
-    // 새로운 key가 현재 node의 key보다 작을 경우,
-    if (key < node.key) {
-        // insert 할 수 있는 position이 아니면 left child에서 다시 insert를 call.
-        // node의 left child가 존재한다면, 
-        if (node->left)
-        {
-            return insert(node->left, key, value);
-            // return ? 안해주면 문제가 생길 것 같다.
-            // 그럼 뭘 return 해주냐..
-        // leaf node이면 left child로 insert.
-        } else {
-            AVLNode<T,U> * newNode(key, value);
-            node->left = newNode;
-            node->height += 1;
-        }
-    // 새로운 key가 현재 node의 key보다 클 경우,
-    } else if (key > node.key) {
-        // node의 right child가 존재한다면,
-        if (node->right)
-        {
-            return insert(node->right, key, value);
-        } else {
-            AVLNode<T,U> * newNode(key, value);
-            node->right = newNode;
-            node->height += 1;
-        }
+    // Perform insertion recursively
+    if (!node)
+        return new AVLNode<T,U>(key, value);
+    if (key < node->key)
+        node->left = insert(node->left, key, value);
+    else if (key > node->key)
+        node->right = insert(node->right, key, value);
     // key가 현재 node의 key와 일치할 경우, value만 replace 한다.
-    } else {
+    else {
         node->value = value;
-        return root;
+        return node;
     }
 
-    // 여기까지 insert 완료. 이제 balance check이 필요하다.
+    // update height of this ancestor nodes
+    node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
 
+    // balance check of this ancestor nodes
+    int balance = getBalance(node);
     
-    
-    
-    //right-left
-
+    //left-left
+    if (balance > 1 && key < node->left->key)
+        return rotate_right(node);
     //right-right
-    
+    if (balance < -1 && key > node->right->key)
+        return rotate_left(node);
     //left-right
-   
+    if (balance > 1 && key > node->left->key) {
+        node->left = rotate_left(node->left);
+        return rotate_right(node);
+    }
     //right-left
+    if (balance < -1 && key < node->right->key) {
+        node->right = rotate_right(node->right);
+        return rotate_left(node);
+    }
+
+    return node;
 }
 
 template<typename T, typename U>
@@ -185,39 +187,98 @@ U AVLTree<T,U>::search(AVLNode<T,U>*& node, const T& key) {
     //TODO
     //return NULL if there are no such key, return value if there is
 
+    if (!node)
+        return "";
     // 현재 node의 key보다 작은 경우,
-    if (key < node->key) {
-        // node의 left child가 존재하면, left child에서 search를 call.
-        if (node->left)
-        {
-            return search(node->left, key);
-        // node의 left child가 존재하지 않으면, 해당 key는 존재하지 않는 것이다.
-        } else {
-            // return ""
-            return NULL;
-        }
-    } else if (key > node->key) {
-        if (node->right)
-        {
-            return search(node->right, key);
-        } else {
-            return NULL;
-        }
+    if (key < node->key)
+        return search(node->left, key);
+    // 현재 node의 key보다 큰 경우,
+    else if (key > node->key)
+        return search(node->right, key);
     // key가 현재 node의 key와 일치하는 경우,
-    } else {
+    else
         return node->value;
-    }
-    
 }
 
 template<typename T, typename U>
 AVLNode<T,U>* AVLTree<T,U>::remove(AVLNode<T,U>*& node, const T& key) {
     //TODO
+    if (!node)
+        return node;
+    
+    if (key < node->key)
+        node->left = remove(node->left, key);
+    else if (key > node->key)
+        node->right = remove(node->right, key);
+    
+    // remove할 node의 key와 일치.
+    else {
+        // node with one or no child
+        if (!node->left || !node->right)
+        {
+            AVLNode<T,U>* temp = node->left ? node->left : node->right;
+
+            // no child
+            if (!temp) {
+                // shallow copy
+                temp = node;
+                node = nullptr;
+            }
+            // one child
+            else
+                // copy contents, except address
+                *node = *temp;
+            
+            delete temp;
+        // node with two children
+        } else {
+            AVLNode<T,U>* temp = min_value_node(node->right);
+
+            node->key = temp->key;
+            node->value = temp->value;
+
+            node->right = remove(node->right, temp->key);
+        }
+    }
+
+    if (!node)
+        return node;
+
+    // update height
+    node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
+
+    // balance check
+    int balance = getBalance(node);
+
+    // left-left
+    if (balance > 1 && getBalance(node->left) >= 0)
+        return rotate_right(node);
+    // right-right
+    if (balance < -1 && getBalance(node->right) <= 0)
+        return rotate_left(node);
+    // left-right
+    if (balance > 1 && getBalance(node->left) < 0) {
+        node->left = rotate_left(node->left);
+        return rotate_right(node);
+    }
+    // right-left
+    if (balance < -1 && getBalance(node->right) > 0) {
+        node->right = rotate_right(node->right);
+        return rotate_left(node);
+    }
+
+    return node;
 }
 
 template<typename T, typename U>
 void AVLTree<T,U>::removeall(AVLNode<T,U>*& node) {
     //TODO
     //for destructor
-        
+    if (node->left)
+        removeall(node->left);
+    if (node->right)
+        removeall(node->right);
+    
+    delete node;
+    return;
 }
